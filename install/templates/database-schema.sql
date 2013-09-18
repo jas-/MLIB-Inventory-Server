f99fe0a4b832e79226af3c1906c620ea92bb89d7
jas- <jason.gerfen@gmail.com>
2013-09-18 13:21:26 -0600

    Wed Sep 18 13:21:25 MDT 2013
    Database:
      Schema:
        Views:
          viewInventoryRMA: Moved date into UTC formatted date type from UNIX_TIMESTAMP

--- install/templates/database-schema.sql
+++ install/templates/database-schema.sql
@@ -127,7 +127,7 @@ VIEW viewInventoryModels AS
 CREATE OR REPLACE DEFINER='{RO}'@'{SERVER}'
  SQL SECURITY INVOKER
 VIEW viewInventoryRMA AS
- SELECT id AS Id, incorrect AS Problem, date AS Date, hostname AS Hostname, model AS Model, sku AS SKU, uuic AS UUIC, serial AS Serial, part AS Part, notes AS Notes FROM rma ORDER BY `date`;
+ SELECT id AS Id, incorrect AS Problem, FROM_UNIXTIME(date, '%Y-%m-%d') AS Date, hostname AS Hostname, model AS Model, sku AS SKU, uuic AS UUIC, serial AS Serial, part AS Part, notes AS Notes FROM rma ORDER BY `date`;
 
 CREATE OR REPLACE DEFINER='{RO}'@'{SERVER}'
  SQL SECURITY INVOKER
