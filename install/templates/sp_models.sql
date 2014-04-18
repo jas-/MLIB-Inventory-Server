d91bb58b86f31a0f6a86ad863b949ea666d5751b
jas- <jason.gerfen@gmail.com>
2014-04-18 16:53:49 -0600

    Stored procedures: Models procs updated to reflect new schema

--- install/templates/sp_models.sql
+++ install/templates/sp_models.sql
@@ -15,26 +15,26 @@ CREATE DEFINER=`{RO}`@`{SERVER}` PROCEDURE `ModelSearch`(IN `s` CHAR(128))
  SQL SECURITY INVOKER
  COMMENT 'Search computer records by model'
 BEGIN
- SELECT * FROM `models` WHERE `model` LIKE s OR `eowd` LIKE s OR `opd` LIKE s OR `description` LIKE s OR `notes` LIKE s ORDER BY `model`;
+ SELECT * FROM `models` WHERE `model` LIKE s OR `description` LIKE s OR `notes` LIKE s ORDER BY `model`;
 END//
 
 DROP PROCEDURE IF EXISTS `ModelAddUpdate`;
-CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ModelAddUpdate`(IN `m` CHAR(128), IN `e` CHAR(32), IN `o` CHAR(32), IN `d` CHAR(128), IN `n` LONGTEXT)
+CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ModelAddUpdate`(IN `m` CHAR(128), IN `d` CHAR(128), IN `n` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Add/Update model'
 BEGIN
- INSERT INTO `models` (`model`, `eowd`, `opd`, `description`, `notes`) VALUES (m, UNIX_TIMESTAMP(e), UNIX_TIMESTAMP(o), d, n) ON DUPLICATE KEY UPDATE `model`=m, `eowd`=UNIX_TIMESTAMP(e), `opd`=UNIX_TIMESTAMP(o), `description`=d, `notes`=n;
+ INSERT INTO `models` (`model`, `description`, `notes`) VALUES (m, d, n) ON DUPLICATE KEY UPDATE `model`=m, `description`=d, `notes`=n;
  SELECT ROW_COUNT() AS affected;
 END//
 
 DROP PROCEDURE IF EXISTS `ModelUpdate`;
-CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ModelUpdate`(IN `i` BIGINT, IN `m` CHAR(128), IN `e` CHAR(32), IN `o` CHAR(32), IN `d` CHAR(128), IN `n` LONGTEXT)
+CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ModelUpdate`(IN `i` BIGINT, IN `m` CHAR(128), IN `d` CHAR(128), IN `n` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Update model'
 BEGIN
- UPDATE `models` SET `model` = m, `eowd` = UNIX_TIMESTAMP(e), `opd` = UNIX_TIMESTAMP(o), `description` = d, `notes` = n WHERE `id` = i LIMIT 1;
+ UPDATE `models` SET `model` = m, `description` = d, `notes` = n WHERE `id` = i LIMIT 1;
  SELECT ROW_COUNT() AS affected;
 END//
 
