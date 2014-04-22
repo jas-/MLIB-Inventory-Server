364926154b27fcbcdc177b6e70a31854d06176e5
jas- <jason.gerfen@gmail.com>
2014-04-22 04:04:09 -0600

    stored proc: working around empty/null values

--- install/templates/monitors-fix.sql
+++ install/templates/monitors-fix.sql
@@ -10,13 +10,13 @@ BEGIN
  BLOCK1: begin
   DECLARE c BOOLEAN DEFAULT FALSE;
 
-  DECLARE i BIGINT DEFAULT NULL;
-  DECLARE h CHAR(128) DEFAULT NULL;
-  DECLARE m CHAR(128) DEFAULT NULL;
-  DECLARE s CHAR(128) DEFAULT NULL;
-  DECLARE sl CHAR(128) DEFAULT NULL;
+  DECLARE i BIGINT;
+  DECLARE h CHAR(128);
+  DECLARE m CHAR(128);
+  DECLARE s CHAR(128);
+  DECLARE sl CHAR(128);
 
-  DECLARE ops CURSOR FOR SELECT * FROM `mbu`;
+  DECLARE ops CURSOR FOR SELECT * FROM `mbu` WHERE `hostname` = '';
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET c = TRUE;
 
   OPEN ops;
@@ -28,37 +28,39 @@ BEGIN
       LEAVE read_loop;
     END IF;
 
-    IF h IS NOT NULL AND h NOT LIKE '' THEN
-      SET @sql = CONCAT('SELECT `id` INTO @hid FROM `hostnames` WHERE `hostname` = "',h,'"');
+    SELECT i AS ID, h AS Hostname, m AS Model, s AS SKU, sl AS Serial;
+
+    IF (h IS NOT NULL OR h != '') THEN
+      SET @sql1 = CONCAT('SELECT `id` INTO @hid FROM `hostnames` WHERE `hostname` = "',h,'"');
     ELSE
-      SET @sql = CONCAT('SELECT 1 INTO @hid');
+      SET @sql1 = CONCAT('SELECT 1 INTO @hid');
     END IF;
 
-    PREPARE stmt FROM @sql;
+    PREPARE stmt FROM @sql1;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
     IF m IS NOT NULL AND m NOT LIKE '' THEN
-      SET @sql = CONCAT('SELECT `id` INTO @mid FROM `models` WHERE `model` = "',m,'"');
+      SET @sql2 = CONCAT('SELECT `id` INTO @mid FROM `models` WHERE `model` = "',m,'"');
     ELSE
-      SET @sql = CONCAT('SELECT 1 INTO @mid');
+      SET @sql2 = CONCAT('SELECT 1 INTO @mid');
     END IF;
 
-    PREPARE stmt FROM @sql;
+    PREPARE stmt FROM @sql2;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
     IF m IS NOT NULL AND m NOT LIKE '' THEN
-      SET @sql = CONCAT('SELECT `id` INTO @wid FROM `warranty` WHERE `id` = "',m,'"');
+      SET @sql3 = CONCAT('SELECT `id` INTO @wid FROM `warranty` WHERE `id` = "',m,'"');
     ELSE
-      SET @sql = CONCAT('SELECT 1 INTO @wid');
+      SET @sql3 = CONCAT('SELECT 1 INTO @wid');
     END IF;
 
-    PREPARE stmt FROM @sql;
+    PREPARE stmt FROM @sql3;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-    SELECT @hid AS HostnameID, @mid AS ModelID, @wid AS WarrantyID, s AS SKU, sl AS Serial;
+--    SELECT h AS Hostname, @hid AS HostnameID, @mid AS ModelID, @wid AS WarrantyID, s AS SKU, sl AS Serial;
 
   END LOOP;
 
