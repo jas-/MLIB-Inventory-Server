0779a400f975509b3f55cea99341afc98b356a35
jas- <jason.gerfen@gmail.com>
2014-04-23 08:59:51 -0600

    stored procedures: Updated computerupdate() proc to reflect database schema updates

--- install/templates/sp_computers.sql
+++ install/templates/sp_computers.sql
@@ -1,7 +1,11 @@
 DELIMITER //
 
+-- Display computer devices using Read-Only view
+-- This procedure limits user access to raw data in the event of a
+-- SQL injection vulnerability
+-- Please see viewInventoryComputers view
 DROP PROCEDURE IF EXISTS `ComputerList`;
-CREATE DEFINER=`{RI}`@`{SERVER}` PROCEDURE `ComputerList`()
+CREATE DEFINER=`{RO}`@`{SERVER}` PROCEDURE `ComputerList`()
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'List all records'
@@ -9,6 +13,10 @@ BEGIN
  SELECT * FROM `viewInventoryComputers`;
 END//
 
+-- Search computer devices using Read-Only view
+-- This procedure limits user access to raw data in the event of a
+-- SQL injection vulnerability
+-- Please see viewInventoryComputers view
 DROP PROCEDURE IF EXISTS `ComputerSearch`;
 CREATE  DEFINER=`{RO}`@`{SERVER}` PROCEDURE `ComputerSearch`(IN `s` LONGTEXT)
  DETERMINISTIC
@@ -18,6 +26,9 @@ BEGIN
  SELECT * FROM `viewInventoryComputers` WHERE `Hostname` LIKE s OR `Model` LIKE s OR `SKU` LIKE s OR `UUIC` LIKE s OR `Serial` LIKE s ORDER BY `hostname` OR s LIKE `eowd` OR s LIKE `opd`;
 END//
 
+-- Add/Updates computer record
+-- Creates new entries for hostname, models & warranty tables if they don't alreay exist
+-- Args: hostname, model, sku, uuic, serial, eowd (end of warranty date), opd (original purchase date) & notes
 DROP PROCEDURE IF EXISTS `ComputerAddUpdate`;
 CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ComputerAddUpdate`(IN `h` CHAR(128), IN `m` CHAR(128), IN `s` CHAR(128), IN `u` CHAR(128), IN `sl` CHAR(128), IN `e` CHAR(32), IN `o` CHAR(32), IN `n` LONGTEXT)
  DETERMINISTIC
@@ -25,27 +36,33 @@ CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ComputerAddUpdate`(IN `h` CHAR(12
  COMMENT 'Add/Update computer'
 BEGIN
 
+  -- Lookup existing ID for hostname, model & warranty
   SELECT `id` INTO @hid FROM `hostnames` WHERE `hostname` = h;
   SELECT `id` INTO @mid FROM `models` WHERE `model` = m;
   SELECT `id` INTO @wid FROM `warranty` WHERE `eowd` = UNIX_TIMESTAMP(e) AND `opd` = UNIX_TIMESTAMP(o);
 
-  SELECT COUNT(*) INTO @exists FROM `viewInventoryComputers` WHERE `SKU` = s OR `UUIC` = u OR `Serial` = sl;
+  -- Determine if we are going to add or update a record
+  SELECT COUNT(*) INTO @exists FROM `computers` WHERE `SKU` = s OR `UUIC` = u OR `Serial` = sl;
 
+  -- If an id doesn't exist for the hostname create one
   IF (@hid <= 0 OR @hid = '' OR @hid IS NULL) THEN
     INSERT INTO `hostnames` (`hostname`) VALUES (h);
     SELECT `id` INTO @hid FROM `hostnames` WHERE `hostname` = h;
   END IF;
 
+  -- If an id doesn't exist for the model create one
   IF (@mid <= 0 OR @mid = '' OR @mid IS NULL) THEN
     INSERT INTO `models` (`model`) VALUES (m);
     SELECT `id` INTO @mid FROM `models` WHERE `model` = m;
   END IF;
 
+  -- If an id doesn't exist for the warranty create one
   IF (@wid <= 0 OR @wid = '' OR @wid IS NULL) THEN
     INSERT INTO `warranty` (`eowd`, `opd`) VALUES (UNIX_TIMESTAMP(e), UNIX_TIMESTAMP(o));
     SELECT `id` INTO @wid FROM `warranty` WHERE `eowd` = UNIX_TIMESTAMP(e) AND `opd` = UNIX_TIMESTAMP(o);
   END IF;
 
+  -- Add or update the computer record
   IF (@exists <= 0) THEN
     INSERT INTO `computers` (`hostname`, `model`, `sku`, `uuic`, `serial`, `warranty`, `notes`) VALUES (@hid, @mid, s, u, sl, @wid, n);
     SELECT ROW_COUNT() AS affected;
@@ -55,18 +72,57 @@ BEGIN
   END IF;
 END//
 
+-- Add/Updates computer record
+-- Updates existing computer, hostname, model & warranty information
+-- Args: id, hostname, model, sku, uuic, serial, eowd (end of warranty date), opd (original purchase date)
 DROP PROCEDURE IF EXISTS `ComputerUpdate`;
-CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ComputerUpdate`(IN `i` BIGINT, IN `h` CHAR(128), IN `m` CHAR(128), IN `s` CHAR(128), IN `u` CHAR(128), IN `sl` CHAR(128), IN `n` LONGTEXT)
+CREATE DEFINER=`{ADMIN}`@`{SERVER}` PROCEDURE `ComputerUpdate`(IN `i` BIGINT, IN `h` CHAR(128), IN `m` CHAR(128), IN `s` CHAR(128), IN `u` CHAR(128), IN `sl` CHAR(128), IN `e` CHAR(32), IN `o` CHAR(32), IN `n` LONGTEXT)
  DETERMINISTIC
  SQL SECURITY INVOKER
  COMMENT 'Update computer record'
-BEGIN
- SELECT `hostname` INTO @Hostname FROM `computers` WHERE `id` = i;
- UPDATE `hostnames` SET `hostname` = h WHERE `hostname` = @Hostname LIMIT 1;
- SELECT ROW_COUNT() INTO @hostname_affected;
- UPDATE `computers` SET `hostname` = h, `model` = m, `sku` = s, `uuic` = u, `serial` = sl, `notes` = n WHERE `id` = i;
- SELECT ROW_COUNT() INTO @computer_affected;
- SELECT @hostname_affected AS hostname_affected, @computer_affected AS computer_affected;
+ComputerUpdate:BEGIN
+
+  -- Lookup existing ID for hostname, model & warranty
+  SELECT `hostname` INTO @hid FROM `computers` WHERE `id` = i;
+  SELECT `model` INTO @mid FROM `computers` WHERE `id` = i;
+  SELECT `warranty` INTO @wid FROM `computers` WHERE `id` = i;
+
+  -- Does a record exist matching the id given?
+  SELECT COUNT(*) INTO @exists FROM `computers` WHERE `id` = i;
+
+  -- If a hostname record doesn't exist update it
+  IF (@hid > 0 OR @hid != '' OR @hid IS NOT NULL) THEN
+    UPDATE `hostnames` SET `hostname` = h WHERE `id` = @hid;
+  ELSE
+    SELECT 0 AS affected;
+    LEAVE ComputerUpdate;
+  END IF;
+
+  -- If a model record exists update it
+  IF (@mid > 0 OR @mid != '' OR @mid IS NOT NULL) THEN
+    UPDATE `models` SET `model` = m WHERE `id` = @mid;
+  ELSE
+    SELECT 0 AS affected;
+    LEAVE ComputerUpdate;
+  END IF;
+
+  -- If a warranty record exists update it
+  IF (@wid > 0 OR @wid != '' OR @wid IS NOT NULL) THEN
+    UPDATE `warranty` SET `eowd` = UNIX_TIMESTAMP(e), `opd` = UNIX_TIMESTAMP(o) WHERE `id` = @wid;
+  ELSE
+    SELECT 0 AS affected;
+    LEAVE ComputerUpdate;
+  END IF;
+
+  -- Add or update the computer record
+  IF (@exists > 0) THEN
+    UPDATE `computers` SET `hostname`=@hid, `model`=@mid, `sku`=s, `uuic`=u, `serial`=sl, `warranty`=@wid, `notes`=n WHERE `id`=i;
+    SELECT ROW_COUNT() AS affected;
+  ELSE
+    SELECT 0 AS affected;
+    LEAVE ComputerUpdate;
+  END IF;
+
 END//
 
 DROP PROCEDURE IF EXISTS `ComputerDelete`;
