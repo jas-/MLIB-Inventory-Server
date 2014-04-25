247b2e4c52ff41a9de45cf255bded16a9adf68ee
jas- <jason.gerfen@gmail.com>
2014-04-25 09:13:53 -0600

    tests & submodules

--- module/Inventory/test/InventoryTest/Controller/IndexControllerTest.php
+++ module/Inventory/test/InventoryTest/Controller/IndexControllerTest.php
@@ -42,6 +42,7 @@ class InventoryControllerTest extends \PHPUnit_Framework_TestCase
 
     $result   = $this->controller->dispatch($this->request);
     $response = $this->controller->getResponse();
+
     $this->assertEquals(200, $response->getStatusCode());
   }
 }
